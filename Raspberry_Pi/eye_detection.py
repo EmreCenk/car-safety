b5d553051a7ee3d8d50f94e5feed2f4385b12157
Emre Cenk <emrecenk9@gmail.com>
2021-10-15 23:20:13 -0400

    commented get_cascades function and wrapped the eye detection into a function

--- Raspberry_Pi/eye_detection.py
+++ Raspberry_Pi/eye_detection.py
@@ -4,6 +4,10 @@
 import cv2
 
 def get_cascades():
+    """
+    Dynamically finds the cascades, and returns 2 cascade objects (face, eye)
+    :return: face_cascade, eye_cascade
+    """
     # Initializing the face and eye cascade classifiers from xml files
     filename1 = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
     face_cascade = cv2.CascadeClassifier(filename1)
@@ -13,55 +17,55 @@ def get_cascades():
 
     return face_cascade, eye_cascade
 
+def start_detection():
+    face_cascade, eye_cascade = get_cascades()
 
-face_cascade, eye_cascade = get_cascades()
 
-
-# Starting the video capture
-cap = cv2.VideoCapture(0)
-ret, img = cap.read()
-
-# we have to pre-define message and color so that my IDE will SHUT UP ABOUT IT. It's fine if you delete the next 2 lines of code, but my IDE loves to bug me about it so here we are
-message = ""
-color = (0, 0, 0) # color is BGR instead of RGB for some reason. Why? bc opencv sucks
-
-while (ret):
+    # Starting the video capture
+    cap = cv2.VideoCapture(0)
     ret, img = cap.read()
-    # Converting the recorded image to grayscale bc it doesn't work if it's not grayscale.
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # Applying filter to remove impurities
-    gray = cv2.bilateralFilter(gray, 5, 1, 1)
+    # we have to pre-define message and color so that my IDE will SHUT UP ABOUT IT. It's fine if you delete the next 2 lines of code, but my IDE loves to bug me about it so here we are
+    message = ""
+    color = (0, 0, 0) # color is BGR instead of RGB for some reason. Why? bc opencv sucks
 
-    # Detecting the face for region of image to be fed to eye classifier
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(200, 200))
+    while (ret):
+        ret, img = cap.read()
+        # Converting the recorded image to grayscale bc it doesn't work if it's not grayscale.
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    message = "No face detected"
-    color = (0, 0, 255)  # red
+        # Applying filter to remove impurities
+        gray = cv2.bilateralFilter(gray, 5, 1, 1)
 
-    for (x, y, w, h) in faces:
+        # Detecting the face for region of image to be fed to eye classifier
+        faces = face_cascade.detectMultiScale(gray, 1.3, 5, minSize=(200, 200))
 
-        #classifying eyes and storing all eyes found in an array:
-        img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-        face_roi = gray[y:y + h, x:x + w]
-        roi_face_clr = img[y:y + h, x:x + w]
-        eyes = eye_cascade.detectMultiScale(face_roi, 1.3, 5, minSize=(50, 50))
+        message = "No face detected"
+        color = (0, 0, 255)  # red
 
-        if (len(eyes) >= 2):
-            message = "eye detected"
-            color = (0, 255, 0) # green
+        for (x, y, w, h) in faces:
 
-        else:
-            message = "No eyes detected"
-            color = (255, 0, 0) #blue
+            #classifying eyes and storing all eyes found in an array:
+            img = cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            face_roi = gray[y:y + h, x:x + w]
+            roi_face_clr = img[y:y + h, x:x + w]
+            eyes = eye_cascade.detectMultiScale(face_roi, 1.3, 5, minSize=(50, 50))
 
-    cv2.putText(img, message, (70, 70), cv2.QT_FONT_BLACK, 3, color, 2)
+            if (len(eyes) >= 2):
+                message = "eye detected"
+                color = (0, 255, 0) # green
 
-    cv2.imshow('img', img)
-    a = cv2.waitKey(1)
-    if (a == ord('q')):
-        break
+            else:
+                message = "No eyes detected"
+                color = (255, 0, 0) #blue
+
+        cv2.putText(img, message, (70, 70), cv2.QT_FONT_BLACK, 3, color, 2)
+
+        cv2.imshow('img', img)
+        a = cv2.waitKey(1)
+        if (a == ord('q')):
+            break
 
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
