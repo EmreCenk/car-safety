e9188cc9ae874261905470c664edb6a24a35010a
Emre Cenk <emrecenk9@gmail.com>
2021-10-15 22:32:53 -0400

    cascades imported dynamically

--- Raspberry_Pi/eye_detection.py
+++ Raspberry_Pi/eye_detection.py
@@ -1,4 +1,19 @@
 
 
-if __name__ == '__main__':
-    print("yay!")
+
+
+import numpy as np
+import cv2
+
+def get_cascades():
+    # Initializing the face and eye cascade classifiers from xml files
+    filename1 = cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+    face_cascade = cv2.CascadeClassifier(filename1)
+
+    filename2 = cv2.data.haarcascades + "haarcascade_eye_tree_eyeglasses.xml"
+    eye_cascade = cv2.CascadeClassifier(filename2)
+
+    return face_cascade, eye_cascade
+
+
+face_cascade, eye_cascade = get_cascades()
