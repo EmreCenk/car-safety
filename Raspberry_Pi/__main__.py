f4cfa5548773bc44dcf6e54be55e3c2fe6c59a19
Callum Irving <callum.irving@protonmail.com>
2021-10-16 12:28:20 -0400

    make python package

--- Raspberry_Pi/__main__.py
+++ Raspberry_Pi/__main__.py
@@ -1,12 +1,14 @@
-import cli
-import on_sleep
+from Raspberry_Pi.cli import parse_arguments
+from Raspberry_Pi.on_sleep import on_sleep
 import os
 
-args = cli.parse_arguments()
 
-if args.sound:
-    print(args.sound)
-    if os.path.isfile(args.sound):
-        on_sleep.on_sleep()
-    else:
-        print("Bad path")
+def main():
+    args = parse_arguments()
+
+    if args.sound:
+        print(args.sound)
+        if os.path.isfile(args.sound):
+            on_sleep()
+        else:
+            print("Bad path")
