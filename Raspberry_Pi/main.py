11654ab6323e4f9963f39482f2f775a088a983d8
Callum Irving <callum.irving@protonmail.com>
2021-10-16 10:46:34 -0400

    slight refactor on_sleep

--- Raspberry_Pi/main.py
+++ Raspberry_Pi/main.py
@@ -1,4 +1,5 @@
 import cli
+import on_sleep
 import os
 
 args = cli.parse_arguments()
@@ -6,6 +7,6 @@ args = cli.parse_arguments()
 if args.sound:
     print(args.sound)
     if os.path.isfile(args.sound):
-        print("Good path")
+        on_sleep.on_sleep()
     else:
         print("Bad path")
--- Raspberry_Pi/on_sleep.py
+++ Raspberry_Pi/on_sleep.py
@@ -3,33 +3,37 @@ import time
 from pydub import AudioSegment
 from pydub.playback import play
 
-filePath = os.path.dirname(os.path.realpath(__file__))
+file_path = os.path.dirname(os.path.realpath(__file__))
 
 files: dict[str, AudioSegment] = {}
 
 
-def playFile(fileName: str, volume: float = 0) -> None:
+def play_file(file_name: str, volume: float = 0) -> None:
     """
     :param fileName: name of audio file in ./media
     :param volume: increase in volume in dB
     :return: void
     """
+
+    # Note(Callum): I changed this because I don't believe that this function
+    # should handle the path stuff
     audio = files.setdefault(
-        fileName, AudioSegment.from_mp3(f"{filePath}/media/{fileName}.mp3")
+        file_name,
+        AudioSegment.from_mp3(file_name),
     )
     audio += volume
 
     play(audio)
 
 
-def onSleep() -> None:
+def on_sleep() -> None:
     for _ in range(10):
-        playFile("alarm", 10)
-        playFile("wake_up", 10)
-        playFile("wake_up", 10)
+        play_file(f"{file_path}/media/alarm.mp3", 10)
+        play_file(f"{file_path}/media/wake_up.mp3", 10)
+        play_file(f"{file_path}/media/wake_up.mp3", 10)
 
         time.sleep(0.5)
 
 
 if __name__ == "__main__":
-    onSleep()
+    on_sleep()
