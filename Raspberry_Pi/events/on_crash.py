8339c93a5b7a87a5612fc3187d9d66dc4ab34fd6
Callum Irving <callum.irving04@gmail.com>
2021-10-16 15:04:00 -0400

    fix no-twilio flag

--- Raspberry_Pi/events/on_crash.py
+++ Raspberry_Pi/events/on_crash.py
@@ -5,15 +5,16 @@ import os
 from twilio.rest import Client
 
 
-account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
-auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
-
-sending = Client(account_sid, auth_token)
 
 
 class sms:
     @staticmethod
     def send(phonenumber, message):
+        account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+        auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+        
+        sending = Client(account_sid, auth_token)
+
         our_phone_number = os.environ.get("TWILIO_PHONE_NUMBER")
         sending.messages.create(from_=our_phone_number, to=phonenumber, body=message)
         print("sent successfully")
